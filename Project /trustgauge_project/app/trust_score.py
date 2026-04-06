1ec7ef8cb9f7b9f2f0ce8b13c73b0c3f5a5dec8c
Parth Naik <pn2325@nyu.edu>
2026-04-06 14:39:43 -0400

    Delete Project  directory

--- Project /trustgauge_project/app/trust_score.py
+++
@@ -1,9 +0,0 @@
-from app.sentiment_analyzer import analyze_sentiment
-
-def calculate_trust_score(reviews):
-    if not reviews:
-        return 0
-    sentiments = [analyze_sentiment(review) for review in reviews]
-    avg_sentiment = sum(sentiments) / len(sentiments)
-    trust_score = round(((avg_sentiment + 1) / 2) * 100, 2)  # Scale 0-100
-    return trust_score
